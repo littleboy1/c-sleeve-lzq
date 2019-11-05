e7abbe34e0b20906b17fa2792acf52020bebd571
812551543@qq.com <812551543@qq.com>
2019-11-05 19:44:47 +0800

    调整优页面应该如何合并HTTP请求

--- model/theme.js
+++ model/theme.js
@@ -3,6 +3,9 @@
 import {Http} from "../utils/http";
 
 class Theme {
+
+    static locationA = 't-1';
+    static locationE = 't-2';
     // 由于request方法是异步的所以所有的层级都是异步调用都需要传入callback
     static async getHomeLocationA() {
         return await Http.request({
