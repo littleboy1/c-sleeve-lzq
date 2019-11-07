826b0bf69cad74b4281816397b5129ccf2bbfecd
812551543@qq.com <812551543@qq.com>
2019-11-07 23:24:22 +0800

    2-4将数据传入组件，创建组件的基本构建

--- pages/home/home.js
+++ pages/home/home.js
@@ -12,6 +12,7 @@ Page({
   data: {
     themeA:null,
     themeE:null,
+    themeESpu:[],
     bannerB:null,
     grid:[],
     activityD:null
@@ -35,12 +36,20 @@ Page({
     await theme.getThemes();
     const themeA = theme.getHomeLocationA();
     const themeE = theme.getHomeLocationE();
+    let themeESpu = [];
+    if (themeE.online){
+      const data = await Theme.getHomeLocationESpu();
+      if (data){
+        themeESpu = data.spu_list.slice(0,8)
+      }
+    }
     const bannerB = await Banner.getHomeLocationB();
     const grid  = await Category.getHomeLocationC();
     const activityD  = await Activity.getHomeLocationD();
     this.setData({
       themeA:themeA,
       themeE,
+      themeESpu,
       bannerB,
       grid,
       activityD
