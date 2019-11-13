69e4bde06d4fe8b751b463581546ce7599cee882
812551543@qq.com <812551543@qq.com>
2019-11-13 21:49:44 +0800

    1-2 liu-ui tag 使用，这里主要是记得使用!important提高外部样式类的等级否则没有效果

--- components/spu-preview/index.js
+++ components/spu-preview/index.js
@@ -11,9 +11,22 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    tags:Array
   },
-
+  observers:{
+    data:function (data) {
+      if (!data){
+        return
+      }
+      if (!data.tags){
+        return
+      }
+      const tags = data.tags.split('$')
+      this.setData({
+        tags
+      })
+    }
+  },
   /**
    * 组件的方法列表
    */
