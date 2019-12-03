1ea7d32512fe4b5abbad14acc771d1ad45aaae76
812551543@qq.com <812551543@qq.com>
2019-12-03 22:45:24 +0800

    新建详情页面跳转页面如果找不到要去APP配置文件下查看一下

--- components/spu-preview/index.js
+++ components/spu-preview/index.js
@@ -37,6 +37,12 @@ Component({
         w:340,
         h:340*height/width
       })
+    },
+    onItemTap(event){
+      const pid = event.currentTarget.dataset.pid
+      wx.navigateTo({
+        url:`/pages/detail/detail?pid=${pid}`,
+      })
     }
   }
 })
